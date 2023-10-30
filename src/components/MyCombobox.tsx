a805c9c55704f37fd1129db8d9c67bdb0a9812e4
Gustavo Pacheco <ryctabo@gmail.com>
2023-10-29 20:23:35 -0500

    feat: create a simple combobox

--- src/components/MyCombobox.tsx
+++ src/components/MyCombobox.tsx
@@ -1,5 +1,37 @@
+'use client'
+
+import { Combobox } from "@headlessui/react"
+import { useState } from "react"
+
+const people = [
+  'Durward Reynolds',
+  'Kenton Towne',
+  'Therese Wunsch',
+  'Benedict Kessler',
+  'Katelyn Rohan',
+]
+
 export function MyCombobox() {
+  const [selectedPerson, setSelectedPerson] = useState(people[0])
+  const [query, setQuery] = useState('')
+
+  const filteredPeople =
+    query === ''
+      ? people
+      : people.filter((person) => {
+        return person.toLowerCase().includes(query.toLowerCase())
+      })
+
   return (
-    <h2>My Combobox</h2>
+    <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+      <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
+      <Combobox.Options>
+        {filteredPeople.map((person) => (
+          <Combobox.Option key={person} value={person}>
+            {person}
+          </Combobox.Option>
+        ))}
+      </Combobox.Options>
+    </Combobox>
   )
 }
