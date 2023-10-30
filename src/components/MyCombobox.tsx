15a1f0543f4d6274d5640e894429a23a1441e46b
Gustavo Pacheco <ryctabo@gmail.com>
2023-10-29 22:10:05 -0500

    fix: more changes

--- src/components/MyCombobox.tsx
+++ src/components/MyCombobox.tsx
@@ -2,6 +2,7 @@
 
 import { Combobox } from '@headlessui/react'
 import { Fragment, ReactNode, useState } from 'react'
+import SelectButtonIcon from './SelectButtonIcon'
 
 const people = [
   'Durward Reynolds',
@@ -24,27 +25,36 @@ export function MyCombobox(): ReactNode {
 
   return (
     <Combobox value={selectedPerson} onChange={setSelectedPerson}>
-      <Combobox.Input
-        autoComplete="off"
-        className="py-2 px-4 rounded shadow shadow-black/40 focus:outline-none"
-        onChange={(event) => setQuery(event.target.value)}
-      />
-      <Combobox.Options className="bg-white mt-2 py-1 rounded shadow shadow-black/30">
-        {filteredPeople.map((person) => (
-          <Combobox.Option key={person} value={person} as={Fragment}>
-            {({ active, selected }) => {
-              const activeClassNames = active ? 'bg-gray-100' : ''
-              const selectedClassNames = selected ? 'bg-cyan-100 text-cyan-700' : ''
-              return (
-                <li
-                  className={`${activeClassNames} ${selectedClassNames} py-2 px-4 cursor-pointer`}
-                >
-                  {person}
-                </li>
-              )
-            }}
-          </Combobox.Option>
-        ))}
+      <div className="relative">
+        <Combobox.Input
+          autoComplete="off"
+          className="py-2 px-4 rounded shadow shadow-black/40 focus:outline-none"
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <Combobox.Button className="absolute right-0 h-full px-1 text-neutral-400">
+          <SelectButtonIcon />
+        </Combobox.Button>
+      </div>
+      <Combobox.Options className="bg-white mt-2 py-1 rounded shadow shadow-black/30 select-none">
+        {filteredPeople.length === 0 && query !== '' ? (
+          <div className="py-2 px-4 cursor-default text-neutral-500">Nothing found.</div>
+        ) : (
+          filteredPeople.map((person) => (
+            <Combobox.Option key={person} value={person} as={Fragment}>
+              {({ active, selected }) => {
+                const activeClassNames = active ? 'bg-gray-100' : ''
+                const selectedClassNames = selected ? 'bg-cyan-100 text-cyan-700' : ''
+                return (
+                  <li
+                    className={`${activeClassNames} ${selectedClassNames} py-2 px-4 cursor-pointer`}
+                  >
+                    {person}
+                  </li>
+                )
+              }}
+            </Combobox.Option>
+          ))
+        )}
       </Combobox.Options>
     </Combobox>
   )
