e412e8f3c071fe49b31bbdea59241f70ff77660d
Gustavo Pacheco <ryctabo@gmail.com>
2023-10-29 21:25:51 -0500

    style: format code by eslint

--- src/components/MyCombobox.tsx
+++ src/components/MyCombobox.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import { Combobox } from "@headlessui/react"
-import { useState } from "react"
+import { Combobox } from '@headlessui/react'
+import { ReactNode, useState } from 'react'
 
 const people = [
   'Durward Reynolds',
   'Kenton Towne',
   'Therese Wunsch',
   'Benedict Kessler',
-  'Katelyn Rohan',
+  'Katelyn Rohan'
 ]
 
-export function MyCombobox() {
+export function MyCombobox(): ReactNode {
   const [selectedPerson, setSelectedPerson] = useState(people[0])
   const [query, setQuery] = useState('')
 
